8035d0266844fc793476a536be150554047e6e75
Pranshu Raj <rajpranshu2222@gmail.com>
2024-07-19 22:24:24 +0530

    ChessBoard

--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -1,3 +1,17 @@
+import ChessBoard from "../components/ChessBoard";
 export default function Game() {
-  return <div>Game</div>;
+  return (
+    <div className="justify-center flex">
+      <div className="pt-8 max-w-screen-lg">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          <div className="grid-cols-span-4 bg-red-200">
+            <ChessBoard />
+          </div>
+          <div>
+            <button>Play</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- frontend/src/screens/Landing.tsx
+++ frontend/src/screens/Landing.tsx
@@ -1,6 +1,8 @@
 import img from "../img/chess.jpeg";
+import { useNavigate } from "react-router-dom";
 
 export default function Landing() {
+  const navigate = useNavigate();
   return (
     <div className="flex justify-center">
       <div className="pt-8 max-w-screen-lg">
@@ -14,7 +16,12 @@ export default function Landing() {
               <h1 className="text-4xl font-bold text-white">Chess khelo!!</h1>
             </div>
             <div className="mt-4 flex justify-center">
-              <button className="px-8 py-4 text-2xl bg-green-500 hover:bg-green-700 text-white font-bold rounded">
+              <button
+                onClick={() => {
+                  navigate("/game");
+                }}
+                className="px-8 py-4 text-2xl bg-green-500 hover:bg-green-700 text-white font-bold rounded"
+              >
                 Play Online
               </button>
             </div>
